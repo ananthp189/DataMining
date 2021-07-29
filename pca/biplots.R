678488f50acaf1eaa4a7a9b04dd5aac8a341524c
ananthp189 <ananthrhul@gmail.com>
2021-07-29 11:04:05 +0100

    final pca interpretation

--- pca/biplots.R
+++ pca/biplots.R
@@ -3,6 +3,9 @@ library(knitr)
 library(corrplot)
 library(TeachingDemos)
 library(ade4)
+library(devtools)
+install_github("vqv/ggbiplot")
+library(ggbiplot)
 
 pairs(USArrests,pch=20,lower.panel = NULL)
 data("USArrests")
@@ -18,3 +21,7 @@ biplot(arrest.pca,xlim=c(-0.3,0.4),cex=.7)
 
 biplot(arrest.pca,xlim=c(-0.3,0.4),xlabs=rep("*",nrow(USArrests)), cex=c(1,0.5),
        arrow.len=0.05)
+biplot(arrest.pca,xlabs=rep("*",nrow(USArrests)), cex=c(1,0.5),arrow.len=0.05,
+       choices=c(3,4))
+gb <- ggbiplot(arrest.pca,circle = T)
+
